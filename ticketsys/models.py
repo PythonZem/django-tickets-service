c6af78ed3774156ad784f834e464a58d15baeaf8
PythonZem <114096673+PythonZem@users.noreply.github.com>
2024-08-13 17:54:32 +0300

    add ticket model

--- ticketsys/models.py
+++ ticketsys/models.py
@@ -1,1 +1,24 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class Ticket(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('in_review', 'In Review'),
+        ('closed', 'Closed'),
+    ]
+
+    title = models.CharField(max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    notes = models.TextField(blank=True, null=True)
+    assigned_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
